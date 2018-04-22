0f62e30184971b42aafae3cfdc9cf7f364d0ea4b
Антон Королев <misantro@yandex.ru>
2018-04-22 22:23:24 +0300

    MA shown

--- hfv.py
+++ hfv.py
@@ -5,7 +5,7 @@ import time
 
 CurrenciesOfInterest = ["BTC-ARK", "BTC-XVG", "BTC-OMG", "BTC-BCC", "BTC-ZEC", "BTC-ETH", "USDT-BTC", "BTC-NEO"]
 MA_FAST_PERIOD = 10
-MA_SLOW_PERIOD = 101
+MA_SLOW_PERIOD = 100
 
 
 bi = Bittrex(None, None, api_version=API_V2_0) 
@@ -13,16 +13,25 @@ market_sums = bi.get_market_summaries()['result']
 markets = list(map(lambda m: m['Market']['MarketName'], market_sums))
 
 def check(bi, market):
-	candles = bi.get_candles(market, 'hour')['result']
+	candles = bi.get_candles(market, 'fiveMin')['result']
 	closes = list(map(lambda c: c['C'], candles))
 
 	ind = Indicators()
 	maslow = ind.movingAverage(closes, MA_SLOW_PERIOD)
 	mafast = ind.movingAverage(closes, MA_FAST_PERIOD)
-	if mafast > maslow * 1.0075:
+	if mafast > maslow:
 		return "UP"
-	elif mafast < maslow * 0.9925:
-		return "DOWN"	
+	elif mafast < maslow:
+		return "DOWN"
+
+def MAoutput(bi, market):
+	candles = bi.get_candles(market, 'hour')['result']
+	closes = list(map(lambda c: c['C'], candles))
+	ind = Indicators()
+	maslow = ind.movingAverage(closes, MA_SLOW_PERIOD)
+	mafast = ind.movingAverage(closes, MA_FAST_PERIOD)
+	return [mafast, maslow]
+					
 
 cyclecounter = 0		
 
@@ -32,7 +41,7 @@ def func():
 	for marketName in CurrenciesOfInterest:
 		string = '{} {}'.format(marketName, 'YES' if check(bi, marketName) == "UP" else 'NO')
 		comparedict[marketName] = [string, string]
-		print(comparedict)
+	print(comparedict)
 	while True:
 		time.sleep(300)
 		global cyclecounter
@@ -48,15 +57,17 @@ def func():
 				comparedict[cur] = comparedict[cur][-1:]
 				comparedict[cur].append(string)
 				if comparedict[cur][0] == comparedict[cur][1]:
-						print(cur, comparedict[cur])
-						print("{} trend is the same".format(cur))
+					print("*****")
+					print(comparedict[cur])
+					print("{} trend is the same".format(cur))
+					print("MA_fast {} MA_slow {}".format(MAoutput(bi, cur)[0], MAoutput(bi, cur)[1]))
 				elif comparedict[cur][0][-3:] == "YES" and comparedict[cur][1][-2:] == "NO":
-					bot.sendMessage(-1001169060108, "{} sell signal".format(cur))				
-					print(cur, comparedict[cur])
+					bot.sendMessage(-1001169060108, "{} *sell signal* {} ".format(cur, MAoutput(bi, cur)))			
+					print(comparedict[cur])
 					print("{} trend has changed".format(cur))
 				elif comparedict[cur][0][-2:] == "NO" and comparedict[cur][1][-3:] == "YES":
-					bot.sendMessage(-1001169060108, "{} buy signal".format(cur))				
-					print(cur, comparedict[cur])
+					bot.sendMessage(-1001169060108, "{} *buy signal* {} ".format(cur, MAoutput(bi, cur)))			
+					print(comparedict[cur])
 					print("{} trend has changed".format(cur))
 
 def main():
