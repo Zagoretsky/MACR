7a03f9f875c1b08d3ecbbfba6e60f1503e876648
Антон Королев <misantro@yandex.ru>
2018-04-28 18:58:18 +0300

    completely reassembled

--- hfv.py
+++ hfv.py
@@ -4,111 +4,81 @@ import telepot
 import time
 
 CurrenciesOfInterest = ["BTC-ZEC", "BTC-ETH", "USDT-BTC", "BTC-BCC", "BTC-OMG"]
-# "BTC-NEO", "BTC-TRX", "BTC-XRP", "BTC-GNT", "BTC-ADA", "BTC-BTG", "BTC-ETC", "BTC-DASH", "BTC-WAVES", "BTC-XMR", "BTC-DOGE", "BTC-LTC", "BTC-ARK", "BTC-XVG", "BTC-OMG", "BTC-BCC", ]
 MA_FAST_PERIOD = 10
 MA_SLOW_PERIOD = 100
 
-
-bi = Bittrex(None, None, api_version=API_V2_0) 
+bi = Bittrex(None, None, api_version=API_V2_0)
 market_sums = bi.get_market_summaries()['result']
 markets = list(map(lambda m: m['Market']['MarketName'], market_sums))
 
-def check(bi, market):
+def masub(market):
 	candles = bi.get_candles(market, 'fiveMin')['result']
 	closes = list(map(lambda c: c['C'], candles))
-
 	ind = Indicators()
 	maslow = ind.movingAverage(closes, MA_SLOW_PERIOD)
 	mafast = ind.movingAverage(closes, MA_FAST_PERIOD)
-	if mafast > maslow * 1.005:
-		return "UP"
-	elif maslow * 0.995 < mafast < maslow * 1.005:
+	masub = (mafast/maslow) - 1
+	return masub
+
+def signal(currency):
+	if masub(currency) > 0.005:
+		return "BUY"
+	elif masub(currency) < -0.005:
+		return "SELL"
+	else:
 		return "EARLY"
-	elif mafast < maslow * 0.995:
-		return "DOWN"
 
-def MAoutput(bi, market):
-	candles = bi.get_candles(market, 'fiveMin')['result']
-	closes = list(map(lambda c: c['C'], candles))
-	ind = Indicators()
-	maslow = ind.movingAverage(closes, MA_SLOW_PERIOD)
-	mafast = ind.movingAverage(closes, MA_FAST_PERIOD)
-	return [mafast, maslow]
-					
+comparedict = dict.fromkeys(CurrenciesOfInterest, [])
 
-cyclecounter = 0
-CurrentSignals = dict.fromkeys(CurrenciesOfInterest, [])
-
-
-def func():
-	bot = telepot.Bot('572875215:AAHeDNnqpu8P5KIKrmeBYM7nx3a9RwZtfz4')
-	comparedict = dict.fromkeys(CurrenciesOfInterest, [])
+def DictFormation():
+	global comparedict
 	for marketName in CurrenciesOfInterest:
-		if check(bi, marketName) == "UP":
-			string = '{} {}'.format(marketName, 'YES')
-		elif check(bi, marketName) == "EARLY":
-			string = '{} {}'.format(marketName, 'EARLY')
-		else:
-			string = '{} {}'.format(marketName, 'NO')
-		comparedict[marketName] = [string, string]
-	print(comparedict)	
+		if len(comparedict[marketName]) == 0:
+			comparedict[marketName] = [signal(marketName), signal(marketName)]
+		elif len(comparedict[marketName]) == 2:
+			comparedict[marketName] = [comparedict[marketName][-1],signal(marketName)]
+
+CurrentSignals = dict.fromkeys(CurrenciesOfInterest, [])
+cyclecounter = 0
+
+def Bot():
 	while True:
-		time.sleep(90)
+		bot = telepot.Bot('572875215:AAHeDNnqpu8P5KIKrmeBYM7nx3a9RwZtfz4')
 		global cyclecounter
-		global CurrentSignals
 		cyclecounter += 1
-		if cyclecounter == 24:
+		if cyclecounter == 30:
 			bot.sendMessage(-1001169060108, "Buddy, I'm working, hope you are doing well too")
 			cyclecounter = 0
 		else:
-			pass	
-		for cur, pair in (comparedict,CurrentSignals):
-			if check(bi, cur) == "UP":
-				string = '{} {}'.format(cur, 'YES')
-			elif check(bi, cur) == "EARLY":
-				string = '{} {}'.format(cur, 'EARLY')
+			pass
+		DictFormation()
+		global comparedict
+		global CurrentSignals
+		for pair in comparedict:
+			if comparedict[pair][0] != "BUY" and comparedict[pair][0] == "BUY" and CurrentSignals[pair] != ["BUY SIGNAL"]:
+				string = (pair, "BUY SIGNAL")
+				print(string)
+				bot.sendMessage(-1001169060108, string)
+				CurrentSignals[pair] = []
+				CurrentSignals[pair] = ["BUY SIGNAL"]
+			elif comparedict[pair][0] != "SELL" and comparedict[pair][0] == "SELL" and CurrentSignals[pair] != ["SELL SIGNAL"]:
+				string = (pair, "SELL SIGNAL")
+				print(string)
+				bot.sendMessage(-1001169060108, string)
+				CurrentSignals[pair] = []
+				CurrentSignals[pair] = ["SELL SIGNAL"]
 			else:
-				string = '{} {}'.format(cur, 'NO')		
-			if len(comparedict[cur]) > 1:
-				comparedict[cur] = comparedict[cur][-1:]
-				comparedict[cur].append(string)
-				if comparedict[cur][0][-2:] != "NO" and comparedict[cur][1][-2:] == "NO":
-					if "SELL" in CurrentSignals[pair]:
-						pass
-					else:
-						bot.sendMessage(-1001169060108, "{} *sell signal* {} ".format(cur, MAoutput(bi, cur)))			
-					print(comparedict[cur])
-					print("{} sell signal".format(cur))
-					if len(CurrentSignals[pair]) == 0:
-						CurrentSignals[pair].append("SELL")
-					elif len(CurrentSignals[pair]) == 1:
-						(CurrentSignals[pair]).pop()
-						CurrentSignals[pair].append("SELL")
-				elif comparedict[cur][0][-3:] != "YES" and comparedict[cur][1][-3:] == "YES":
-					if "BUY" in CurrentSignals[pair]:
-						pass
-					else:
-						bot.sendMessage(-1001169060108, "{} *buy signal* {} ".format(cur, MAoutput(bi, cur)))			
-					print(comparedict[cur])
-					print("{} buy signal".format(cur))
-					if len(CurrentSignals[pair]) == 0:
-						(CurrentSignals[pair]).append("BUY")
-					elif len(CurrentSignals[pair]) == 1:
-						(CurrentSignals[pair]).pop()
-						CurrentSignals[pair].append("BUY")
-				else:
-					print("*****")
-					print(comparedict[cur])
-					print("{} trend is the same".format(cur))
-					print("MA_fast {} MA_slow {}".format(MAoutput(bi, cur)[0], MAoutput(bi, cur)[1]))
-		print(CurrentSignals)		
+				print(pair, "Trend is the same")
+		print(comparedict)
+		print(CurrentSignals)
+		time.sleep(180)
 
-def main():
-	try:
-		func()
-	except:
-		pass
+Bot()
 
-
-while True:
-	main()
+# def main():
+# 	try:
+# 		Bot()
+# 	except:
+# 		pass
+# while True:
+# 	main()
